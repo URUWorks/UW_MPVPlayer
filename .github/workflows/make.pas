c286be8e944fac460e6fcb42e527f016d256803d
Artem V. Ageev <artem.v.ageev@gmail.com>
2025-07-02 18:45:25 +0300

    fix CI

--- .github/workflows/make.pas
+++ .github/workflows/make.pas
@@ -213,7 +213,7 @@ begin
   try
     if ParamCount > 0 then
       case ParamStr(1) of
-        'build': BuildAll(['BGRABitmap', 'FPSpreadsheet', 'ATFlatControls', 'EncConv', 'ATSynEdit']);
+        'build': BuildAll([]);
         else
           OutLog(etError, ParamStr(1));
       end;
